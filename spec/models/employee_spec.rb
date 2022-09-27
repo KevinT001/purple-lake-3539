90e676baff5474fa3d8061b1257070dc87f22027
Kevin Ta <kvnta@yahoo.com>
2022-09-27 09:55:48 -0600

    Correct mmodel tests

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
   describe 'relationships' do
-    it { should belong_to :department }
+    
 
     it { should have_many :employeetickets}
 
-    it { should have_many(:tickets).through(:employeetickets}
+    it { should have_many(:tickets).through(:employeetickets)}
 
   end
 end
---
+++ spec/models/ticket_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Ticket, type: :model do
+  describe 'relationships' do
+   
+
+    it { should have_many(:employeetickets)}
+
+    it { should have_many(:employees).through(:employeetickets)}
+
+  end
+end
